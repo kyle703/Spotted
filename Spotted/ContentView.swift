e3d18ee9c7819b987e5bf1834731d77c92dc9bde
Kyle Thompson <thompson.kyle.m@gmail.com>
2020-10-04 09:05:03 -0400

    Initial Commit

---
+++ Spotted/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotted
+//
+//  Created by Kyle Thompson on 10/4/20.
+//  Copyright © 2020 Kyle Thompson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
